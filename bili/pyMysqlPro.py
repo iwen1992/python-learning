a5f9bba0b649803e264b8184520b66112b1f13f8
iwen1992 <3066420900@qq.com>
2021-02-26 16:44:51 +0800

    爬取腾讯疫情数据保存数据库

--- bili/pyMysqlPro.py
+++ bili/pyMysqlPro.py
@@ -1,4 +1,7 @@
+import pymysql
+import time
 import json
+import traceback
 import requests
 def getDetail():
     url = "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5"
@@ -26,5 +29,39 @@ def getDetail():
             dead = city_info['total']['dead']
             details.append([update_time,provice,city,confirm,confirm_add,heal,dead])
     return details,history
-res = getDetail()
-print(res)
+def get_conn():
+    conn = pymysql.connect(host="localhost",user="root",password="Wg83saSaGcHHezfK",db="feiyan",charset="utf8")
+    cursor = conn.cursor()
+    return conn,cursor
+def close_conn(conn,cursor):
+    if conn:
+        conn.close()
+    if cursor:
+        cursor.close()
+def updata_details():
+    cursor = None
+    conn = None
+    try:
+        li = getDetail()[0]
+        conn,cursor=get_conn()
+        sql = "insert into detail(update_time,provice,city,confirm,confirm_add,heal,dead) values(%s,%s,%s,%s,%s,%s,%s)"
+        sql_query = "select %s=(select update_time from detail order by id desc limit 1)"
+        cursor.execute(sql_query,li[0][0])
+        if not cursor.fetchone()[0]:
+            print("开始更新数据")
+            for item in li:
+                cursor.execute(sql,item)
+            conn.commit()
+            print("更新数据完毕")
+        else:
+            print("已是最新数据完毕")
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn,cursor)
+def updata_history():
+    cursor = None
+    conn = None
+    conn,cursor = get_conn()
+updata_details()
+
